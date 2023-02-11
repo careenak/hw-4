cf5673fff4d0e973650672b7f258bea6896b515f
Karina <lyakhova_karina@list.ru>
2023-02-11 21:50:49 +0300

    задача 27

---
+++ 27/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int n = ReadInt("Введите число: ");
+int k = Kol(n);
+SumNumb(n, k);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция, которая считает количество цифр в числе
+int Kol(int a)
+{
+    int i = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        i++;
+    }
+    return i;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumb(int x, int kl)
+{
+    int sum = 0;
+    for (int i = 1; i <= kl; i++)
+    {
+        sum += x % 10;
+        x /= 10;
+    }
+    Console.WriteLine(sum);
+}
